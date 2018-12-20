b1905cff46d737ffdb304529b8ca003fcb19a8a9
Andrey Gramzin <andrew46446@yandex.ru>
2018-12-20 13:20:01 +0300

    Revert "Изменено имя проекта taskList.Service ( попытка 2)"
    This reverts commit 713e4e656e0bc4be34ef43c2555816ef26af771f.

--- TaskList.Servise/TaskList.Servise/Class1.cs
+++ TaskList.Servise/TaskList.Servise/Class1.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TaskList.Service
+namespace TaskList.Servise
 {
 
     public class NavigationService : INavigationService
