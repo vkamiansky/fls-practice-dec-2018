aa0a5c2a5f4f095e3ad36f17c62d10b8f01fd57c
Анна Мишкова <anyamishkova@gmail.com>
2018-12-20 13:18:11 -0800

    Solved conflict with main brunch

--- UWP/TaskList.ViewModel/TaskList.ViewModel/MainViewModel.cs
+++ UWP/TaskList.ViewModel/TaskList.ViewModel/MainViewModel.cs
@@ -4,7 +4,7 @@ using TaskList.Service;
 
 namespace TaskList.ViewModel
 {
-    public class MainViewModel : IMainViewModel
+    public class MainViewModel : IMainViewModel 
     {
         NavigationService NavigationService { set; get; }        
     }
