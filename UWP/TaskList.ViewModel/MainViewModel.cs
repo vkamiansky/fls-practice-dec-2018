4d5cd52a1d8a384a36d901649c9c48d8650fd6d0
Анна Мишкова <anyamishkova@gmail.com>
2018-12-20 16:03:10 -0800

    #4 Added NavigationService property (needs TaskList.Service modification)

--- UWP/TaskList.ViewModel/MainViewModel.cs
+++ UWP/TaskList.ViewModel/MainViewModel.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Text;
 using TaskList.Interface;
+using TaskList.Service;
 
 namespace TaskList.ViewModel
 {
     class MainViewModel : IMainViewModel
     {
+        NavigationService NavigationService { set; get; }
     }
 }
