5958689ae565ccc0b9aae656121d75efd878b2d8
dposokhov <p-blank12@yandex.ru>
2018-12-22 21:18:09 +0300

    Added using TaskList.Model; in MainViewModel

--- UWP/TaskList.View/TaskList.View/Class1.cs
+++ UWP/TaskList.View/TaskList.View/Class1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using TaskList.Interface;
 
 namespace TaskList.View
 {
--- UWP/TaskList.ViewModel/MainViewModel.cs
+++ UWP/TaskList.ViewModel/MainViewModel.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using TaskList.Interface;
+using TaskList.Model;
 
 namespace TaskList.ViewModel
 {
