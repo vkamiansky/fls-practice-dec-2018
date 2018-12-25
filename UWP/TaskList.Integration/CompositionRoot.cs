075bc879d316d37516ff32170149a9b13c3bcf83
Danila <dds2412@mail.ru>
2018-12-25 15:21:43 +0300

    Добавлен Autofac. Добавлена базовая реализация CompositionRoot

--- UWP/TaskList.Integration/CompositionRoot.cs
+++ UWP/TaskList.Integration/CompositionRoot.cs
@@ -1,8 +1,50 @@
 ﻿using System;
+using Autofac;
+using TaskList.Interface;
+using TaskList.Service;
+using TaskList.View;
+using TaskList.ViewModel;
+using Windows.UI.Xaml.Controls;
 
 namespace TaskList.Integration
 {
-    public class Class1
+    public class CompositionRoot
     {
+        private static IContainer container;
+
+        public static IMainViewModel GetMainViewModel(Frame frame)
+        {
+            if (container == null)
+                container = BuildContainer();
+
+            var mainViewModel = container.Resolve<IMainViewModel>();
+            mainViewModel.NavigationService = container.
+                Resolve<INavigationService>(
+                new NamedParameter("frame", frame));
+
+            return mainViewModel;
+        }
+
+        private static IContainer BuildContainer()
+        {
+            var containerBuilder = new ContainerBuilder();
+
+            // Место для добавления сервисов
+            containerBuilder.
+                RegisterType<NavigationService>().
+                As<INavigationService>();
+
+            // Добавление страниц
+            containerBuilder.
+                RegisterType<MainPage>().
+                Keyed<Page>(PageKeys.MainPage);
+
+            // Добавление MainViewModel
+            containerBuilder.
+                 RegisterType<MainViewModel>().
+                 As<IMainViewModel>();
+
+            return containerBuilder.Build();
+        }
     }
 }
