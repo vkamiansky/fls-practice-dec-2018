8bebcbe35cceedcbd89897d1cd643eba5d8a5432
Danila <dds2412@mail.ru>
2018-12-25 15:43:56 +0300

    Добавлен вызов CompositionRoot.GetMainViewModel(rootFrame).StartNavigation()

--- UWP/TaskList/App.xaml.cs
+++ UWP/TaskList/App.xaml.cs
@@ -14,6 +14,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using TaskList.Integration;
 
 namespace TaskList
 {
@@ -66,6 +67,7 @@ namespace TaskList
                     // When the navigation stack isn't restored navigate to the first page,
                     // configuring the new page by passing required information as a navigation
                     // parameter
+                    //CompositionRoot.GetMainViewModel(rootFrame).StartNavigation();
                     rootFrame.Navigate(typeof(MainPage), e.Arguments);
                 }
                 // Ensure the current window is active
