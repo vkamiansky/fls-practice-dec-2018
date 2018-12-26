80998af108d60b0efeccf914f359efcdc64cdc7f
Danila <dds2412@mail.ru>
2018-12-26 16:25:44 +0300

    Добавлена тестовая версия TaskModel, содержащая информацию о таске.

---
+++ UWP/TaskList.Model/TaskModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace TaskList.Model
+{
+    public class TaskModel : INotifyPropertyChanged
+    {
+        private string title;
+        private string description;
+
+        public string Title
+        {
+            get => title;
+            set
+            {
+                title = value;
+                OnPropertyChanged("Title");
+            }
+        }
+
+        public string Description
+        {
+            get => description;
+            set
+            {
+                description = value;
+                OnPropertyChanged("Description");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
