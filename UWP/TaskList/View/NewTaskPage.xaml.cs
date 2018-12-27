7778457b9d097856ccea3f6fff8cc8353c649d74
Danila <dds2412@mail.ru>
2018-12-28 02:53:43 +0300

    Заголовок и текст кнопки заданы статически. Удалены свойства текста заголовка и текста кнопки

--- UWP/TaskList/View/NewTaskPage.xaml.cs
+++ UWP/TaskList/View/NewTaskPage.xaml.cs
@@ -24,7 +24,7 @@ namespace TaskList
     {
         public NewTaskPage()
         {
-            this.DataContext = new TaskList.TestViewModel.TestNewTaskViewModel();
+            //this.DataContext = new TaskList.TestViewModel.TestNewTaskViewModel();
             this.InitializeComponent();
         }
 
